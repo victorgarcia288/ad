9460eafc525101a6fa2073c42e53193cedbf2269
victorgarcia288 <victorgarcia2688@gmail.com>
2017-10-04 12:55:33 +0200

    iniciada CCategoria (lectura de categoria y nuevo)

---
+++ CCategoria/CCategoria/MainWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Gtk;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+using CCategoria;
+
+public partial class MainWindow : Gtk.Window
+{
+    
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        
+
+        this.Build();
+
+        string connectionString = "server=localhost;database=dbprueba;user=root;password=sistemas";
+		App.Instance.Connection = new MySqlConnection(connectionString);
+		App.Instance.Connection.Open();
+
+		IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
+		dbCommand.CommandText = "select * from categoria order by id";
+		IDataReader dataReader = dbCommand.ExecuteReader();
+		
+        treeview1.AppendColumn("id", new CellRendererText(), "text", 0);
+        treeview1.AppendColumn("nombre", new CellRendererText(), "text", 1);
+        ListStore listStore = new ListStore(typeof(string), typeof(string));
+        treeview1.Model = listStore;
+
+        while (dataReader.Read())
+            listStore.AppendValues(dataReader["nombre"]);
+		dataReader.Close();
+
+        newAction.Activated += delegate {
+            new CategoriaWindow();
+        };
+
+	}
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        App.Instance.Connection.Close();
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
